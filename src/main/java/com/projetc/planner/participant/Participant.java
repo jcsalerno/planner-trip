4271104a9a954579d4f043f1cb483874cbdbc0ba
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-14 17:58:57 -0300

    feat: created the entity that will represent a participant

---
+++ src/main/java/com/projetc/planner/participant/Participant.java
@@ -0,0 +1,35 @@
+package com.projetc.planner.participant;
+
+import com.projetc.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
