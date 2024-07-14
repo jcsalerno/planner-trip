808c9de2761b3af420a73733c4711e9c210a47ad
Julio Cesar <contato.jcsalerno@gmail.com>
2024-07-14 17:35:50 -0300

    feat: create trip confirmation endpoint get/trips/id/confirm

--- src/main/java/com/projetc/planner/trip/TripController.java
+++ src/main/java/com/projetc/planner/trip/TripController.java
@@ -1,10 +1,8 @@
 package com.projetc.planner.trip;
-
 import com.projetc.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,11 +20,8 @@ public class TripController {
     @PostMapping
     public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
         Trip newTrip = new Trip(payload);
-
         this.repository.save(newTrip);
-
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
-
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
 
     }
@@ -48,13 +43,25 @@ public class TripController {
             rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at()));
             rawTrip.setStartsAt(LocalDateTime.parse(payload.starts_at()));
             rawTrip.setDestination(payload.destination());
-
             this.repository.save(rawTrip);
-
             return ResponseEntity.ok(rawTrip);
-
         }
 
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id) {
+        Optional<Trip> tripOptional = this.repository.findById(id);
+
+        if(tripOptional.isPresent()) {
+            Trip rawTrip = tripOptional.get();
+            rawTrip.setIsConfirmed(true);
+            this.repository.save(rawTrip);
+            this.participantService.triggerConfirmationEmailToParticipants(id);
+            return ResponseEntity.ok(rawTrip);
+        }
         return ResponseEntity.notFound().build();
 
     }
